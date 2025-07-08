2c3a59980c6b6a87d2aeffc51ebb78aaa155d3df
Your Name <you@example.com>
2025-07-07 22:49:18 -0300

    First commit: project startd

---
+++ backend/src/main/java/com/soareslipe/Pinpsi/PinpsiApplication.java
@@ -0,0 +1,13 @@
+package com.soareslipe.Pinpsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinpsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinpsiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soareslipe/Pinpsi/PinpsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soareslipe.Pinpsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinpsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
